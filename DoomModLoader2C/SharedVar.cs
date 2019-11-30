360c19a4a43de4776c7d1b02fa3152a26e0fba76
Premo36 <50420475+Premo36@users.noreply.github.com>
2019-11-30 18:27:06 +0100

    DML v2.2 RELEASE - 30/11/2019

--- DoomModLoader2C/SharedVar.cs
+++ DoomModLoader2C/SharedVar.cs
@@ -7,7 +7,7 @@ namespace DoomModLoader2
 {
     public static class SharedVar
     {
-        public static string LOCAL_VERSION { get; } = "2.2 (RC #2 , Beta #10)";
+        public static string LOCAL_VERSION { get; } = "2.2";
         public static bool CHECK_FOR_UPDATE { get; set; }
         public  static  string UrlVersion { get; } = @"https://drive.google.com/uc?export=download&id=1lXEz8sXjJGma0Q34Bj-mpjTtVW8RK-W4";
 
